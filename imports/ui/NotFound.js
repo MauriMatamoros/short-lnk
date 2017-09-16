506402734a70c5bc59ee41041187e6cb76034ced
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-09-16 14:25:22 -0600

    refactoring

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  render() {
-    return(
-      <p>NotFound component</p>
-    );
-  };
+export default () => {
+  return(
+    <p>NotFound component</p>
+  );
 };
